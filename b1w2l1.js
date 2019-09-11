12f4b16f09e9a5d97c7c45a2b90c926118b6e3a0
Unknown <unknown@example.com>
2019-09-11 11:00:54 +0200

    veel

---
+++ b1w2l1.js
@@ -0,0 +1,4 @@
+document.write('Noah Dubbelman');//commentaar
+document.write('ICT Applicatie en media ontwikkelaar');//commentaar
+
+prompt('Je naam');
---
+++ b1w2l1/b1w2l1 kopie.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+document.write('1x6 =' (1*6));
+
+</body>
+</html>
